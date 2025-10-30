bd575fdc2c30daa154ce6682bead4137f9372be3
Omar Amer <oiamer4778@gmail.com>
2025-10-30 20:42:09 +0200

    feat: added LinuxMemoryMonitor.
    The methods are not implemented yet.

---
+++ ServerStatisticsCollectionService/MemoryMonitors/LinuxMemoryMonitor.cs
@@ -0,0 +1,14 @@
+namespace ServerStatisticsCollectionService.MemoryMonitors;
+
+public class LinuxMemoryMonitor : IMemoryMonitor
+{
+    public double GetMemorUsage()
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetAvailableMemory()
+    {
+        throw new NotImplementedException();
+    }
+}
