4b15939027f8fd248c2a09fcfaabd1c451e93c32
Omar Amer <oiamer4778@gmail.com>
2025-11-01 11:28:56 +0200

    chore: Program class setup.

--- ServerStatisticsCollectionService/Program.cs
+++ ServerStatisticsCollectionService/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace ServerStatisticsCollectionService;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ServerStatisticsCollectionService.RabbitMQSenders;
+using ServerStatisticsCollectionService.ServerStatisticsCollectorCreators;
+
+namespace ServerStatisticsCollectionService;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-       
+        var host = Host.CreateDefaultBuilder(args) .ConfigureServices((context, services) => 
+            { 
+               services.AddTransient<IServerStatisticsCollectorCreator, ServerStatisticsCollectorCreator>(); 
+               services.AddTransient<IRabbitMQSender, RabbitMQSender>(); 
+                services.AddSingleton<ServiceLoop>(); 
+            }) .Build();
+
+        await using var scope = host.Services.CreateAsyncScope();
+        var service = host.Services.GetRequiredService<ServiceLoop>();
+        await service.StartService();
     }
 }
